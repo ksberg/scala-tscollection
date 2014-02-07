f4f5412a2ae0875a65f5b8365662bd1dbeddf63f
Kevin Berg <ksberg@gmail.com>
2014-02-06 16:56:45 -0800

    Test
    Eclipse vs IntelliJ - Idea typing requires more methods be implemented
    on IndexedSeq implementation.

--- src/main/scala/bitzguild/scollection/transform/SeriesFunction.scala
+++ src/main/scala/bitzguild/scollection/transform/SeriesFunction.scala
@@ -1,8 +1,35 @@
 package bitzguild.scollection.transform
 
-import bitzguild.scollection.LeftSeq
+import bitzguild.scollection.{LeftSeq,LeftView}
 
 trait LeftSeqFunction[A,LS >: LeftSeq[A]] {
   def init(domain: LS) : Unit
   def apply(domain: LS): A
+}
+
+abstract class LeftFunctionCache[A: Numeric](
+    domain : LeftSeq[A],
+    cacheFunction: LeftSeqFunction[A, LeftSeq[A]],
+    lookback : Int
+    ) extends LeftSeq[A] {
+
+  val cache = domain.another
+  val dview = domain.view(lookback)	// val implies that view must be mutable in order to increment
+  def length = domain.length
+  def initD = {
+      cacheFunction.init(dview) 
+  }
+  def apply(index: Int) = {
+    while(dview.hasNext) {
+    }
+    cache(index)	// NOT!
+  }
+  
+  // prevent ops
+  def +=(elem: A): LeftSeq[A]
+  def ++=(col: Traversable[A]) : LeftSeq[A]
+
+  def view(len: Int) = cache.view(len)
+  def another = cache.another
+  
 }
